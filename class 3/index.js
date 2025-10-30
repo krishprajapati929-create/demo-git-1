6fd9a6a66864f8c333dd2e1a2c385c35eec1f84c
green india <krishprajapati929@gmail.com>
2025-10-31 02:59:15 +0530

    how to use local storage

--- class 3/index.js
+++ class 3/index.js
@@ -10,6 +10,7 @@ addbtn.addEventListener("click",()=>{
     }else{
         const li = document.createElement("li")
         li.textContent=tasktext;
+        localStorage.setItem("firstname",tasktext)
         let newbtn = document.createElement("button")
         newbtn.textContent = "delete"
         newbtn.addEventListener("click",()=>{
@@ -18,7 +19,27 @@ addbtn.addEventListener("click",()=>{
         li.appendChild(newbtn)
         tasklist.appendChild(li)
         input.value="";
-    }
+    };
     
 
-})
+});
+localStorage.setItem("firstname", JSON.stringify({name:"krish",age:22,gender:"male"}));
+let value = localStorage.getItem('firstname') 
+console.log(value)
+localStorage.setItem("firstname","krish",)
+localStorage.setItem("secondname","krish",)
+
+sessionStorage.setItem("lastname","smith");
+
+
+// document.querySelector("#grandparent").addEventListener("click",()=>{
+//     console.log("grandparent")
+// })
+// document.querySelector("#parent").addEventListener("click",(event)=>{
+//     console.log("parent")
+//     event.stopImmediatePropagation()  
+//     // \^<== this is stopping propegation
+// })
+// document.querySelector("#btn").addEventListener("click",()=>{
+//     console.log("button")
+// })
