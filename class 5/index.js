c90d7fb3a242e8ea24139953b4612d9d14719791
green india <krishprajapati929@gmail.com>
2025-11-14 15:54:07 +0530

    compleate js for card but did'not compleate css

--- class 5/index.js
+++ class 5/index.js
@@ -8,10 +8,12 @@ const carousalImages = [
    , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO9Q4RgS84bHZXfKHgYngwhUuKWafoXm4kqQ&s"
 ]
 currentslide = 0
+let allmovies = []
 
-const indexcontainer = document.querySelector(".display-container")
+const displaycontainer = document.querySelector(".display-container")
+const cardContainer = document.querySelector(".card-container")
 function initcrousal(){
-    indexcontainer.innerHTML=""
+    displaycontainer.innerHTML=""
     carousalImages.forEach((imageurl,index)=>{
         const slide = document.createElement("div")
     slide.className="slidecrousal"
@@ -23,7 +25,7 @@ function initcrousal(){
         newimg.src=imageurl
         newimg.alt=`slide${index+1}`
         slide.appendChild(newimg)
-        indexcontainer.appendChild(slide)
+        displaycontainer.appendChild(slide)
     })
 }
 
@@ -37,14 +39,57 @@ function liveimage(){
 }
 function autoNext(){
     currentslide=(currentslide+1)%carousalImages.length
-    console.log(currentslide)
+
     liveimage()
 }
 function changeSlide(direction){
     currentslide = (currentslide+direction+carousalImages.length)%carousalImages.length
     liveimage()
 }
+async function loadmovies(){
+    try {
+    const response = await fetch("http://localhost:3001/Movies") 
+    if(!response){
+        console.log("failed to fached movies")
+    }
+    allmovies = await response.json() 
+    console.log("data",allmovies)
+    displaymovies()
+    } catch (err) {
+        console.log(err)
+    }
+}
+function displaymovies(){
+    if(!cardContainer){
+        console.log("movies container is not found")
 
+    }
+    if(!allmovies||allmovies.length===0){
+        displaycontainer.innerHTML=" <h1 style='color: white; text-align: center;'>no Movies availavle</h1>"
+    }
+    allmovies.forEach(movie=>{
+    const carddiv = document.createElement("div")
+    carddiv.className="movie-card"
+    carddiv.innerHTML=`
+        <div class="movieposter">
 
+            <img class="img"src=${movie.poster} alt=${movie.title}>
+        </div>
+        <div class="imfo">
+            <div class="movietitle">${movie.title}</div>
+            <div class="movieid"></div>
+            <div class="moviecetegory">${movie.category}</div>
+            <div class="movierank">${movie.rank}</div>
+            <div class="movieyear">${movie.year}</div>
+            <button class="card-cart">cart</button>
+            <button class="card-favourite">favourite</button>
+        </div>
+        
+    `
+    cardContainer.appendChild(carddiv)
+    })
+}
+
+loadmovies()
 setInterval(autoNext,2000)
 initcrousal()
